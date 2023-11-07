efa764acb700993ae2056213050b3731548fff0f
oscar1223 <oscararauzp@gmail.com>
2023-11-07 16:25:55 +0100

    +

--- exercise3.py
+++ exercise3.py
@@ -1,1 +1,6 @@
 '''Recursive Neural Network'''
+''' La estructura de una red neuronal artificial es relativamente simple y se refiere principalmente a la multiplicación de matrices. 
+Durante el primer paso, las entradas se multiplican por pesos inicialmente aleatorios, y sesgo, 
+transformados con una función de activación y los valores de salida se utilizan para hacer una predicción. 
+Este paso da una idea de lo lejos que está la red de la realidad. '''
+
